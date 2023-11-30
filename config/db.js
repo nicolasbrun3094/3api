d242f480e46b57f843820498e7647db036767c06
Med Marwen Meddeb <marwen.meddeb1812@gmail.com>
2023-11-30 13:58:40 +0100

    - DB connection string - Passport.js pour gérer l'authentification

--- config/db.js
+++ config/db.js
@@ -1,4 +1,21 @@
 /*
-Ce fichier contient la configuration pour la base de données (par exemple, MongoDB). 
+Ce fichier contient la configuration pour la base de données (MongoDB). 
 Il peut inclure l'URL de la base de données, les paramètres de connexion, etc.
 */
+
+// config/db.js
+const mongoose = require('mongoose');
+
+// BD locale Mongo (raildraod)
+const databaseUrl = 'mongodb://localhost/railroad';
+
+mongoose
+    .connect(databaseUrl)
+    .then(() => {
+      console.log('Connexion à MongoDB RaildRoad réussie !');
+    })
+    .catch((error) => {
+      console.error('Connexion à MongoDB RailRoad échouée ! : ', error);
+    });
+
+module.exports = mongoose;
---
+++ config/passport.js
@@ -0,0 +1,13 @@
+/* Fichier de configuration pour Passport.js, qui est utilisé pour gérer l'authentification (Passport-local-mongoose) */ 
+
+// config/passport.js
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('../models/user');
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+module.exports = passport;
