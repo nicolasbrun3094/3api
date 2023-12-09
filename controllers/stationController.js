765d5d44ebd3f844d1eb4806a8ec0f5012f5fdf2
Nicolas <nicolasbrun3094@hotmail.com>
2023-12-09 11:49:06 +0100

    stationCtrl ok

--- controllers/stationController.js
+++ controllers/stationController.js
@@ -4,37 +4,35 @@
 
 // controllers/stationController.js
 const Station = require('../models/station');
+const Train = require('../models/train');
+
+// Fonction d'aide pour vérifier si l'utilisateur est un administrateur
+const isAdmin = (req) => {
+    return req.userData && req.userData.role === 'admin';
+};
 
 // Créer une nouvelle station
 exports.createStation = async (req, res) => {
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Unauthorized: Only administrators can perform this action." });
+    }
     try {
-        // Les données de la station doivent être dans req.body
         const { name, open_hour, close_hour } = req.body;
-
-        // Création d'une nouvelle instance de Station avec les données reçues
         const newStation = new Station({ name, open_hour, close_hour });
-
-        // Sauvegarde de la nouvelle station dans la base de données
         const station = await newStation.save();
-
-        // Envoi de la réponse avec la station créée
         res.status(201).json(station);
     } catch (error) {
-        // Gestion des erreurs
         res.status(500).json({ message: error.message });
     }
 };
 
-// Récupérer la liste de toutes les stations
+// Récupérer la liste de toutes les stations avec tri par nom
 exports.getAllStations = async (req, res) => {
     try {
-        // Récupération de toutes les stations de la base de données
-        const stations = await Station.find();
-
-        // Envoi de la réponse avec la liste des stations
+        const sortField = req.query.sort || 'name'; // Tri par nom par défaut
+        const stations = await Station.find().sort(sortField);
         res.json(stations);
     } catch (error) {
-        // Gestion des erreurs
         res.status(500).json({ message: error.message });
     }
 };
@@ -43,67 +41,55 @@ exports.getAllStations = async (req, res) => {
 exports.getStationById = async (req, res) => {
     try {
         const { stationId } = req.params;
-
-        // Récupération d'une station spécifique par son ID
         const station = await Station.findById(stationId);
-
         if (!station) {
             return res.status(404).json({ message: "Station not found" });
         }
-
-        // Envoi de la réponse avec la station trouvée
         res.json(station);
     } catch (error) {
-        // Gestion des erreurs
         res.status(500).json({ message: error.message });
     }
 };
 
 // Mettre à jour une station spécifique
 exports.updateStation = async (req, res) => {
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Unauthorized: Only administrators can perform this action." });
+    }
     try {
         const { stationId } = req.params;
         const { name, open_hour, close_hour } = req.body;
-
-        // Mise à jour de la station spécifiée avec les nouvelles données
-        const updatedStation = await Station.findByIdAndUpdate(
-            stationId, 
-            { name, open_hour, close_hour },
-            { new: true } // L'option { new: true } assure que la station retournée est celle après mise à jour
-        );
-
+        const updatedStation = await Station.findByIdAndUpdate(stationId, { name, open_hour, close_hour }, { new: true });
         if (!updatedStation) {
             return res.status(404).json({ message: "Station not found" });
         }
-
-        // Envoi de la réponse avec la station mise à jour
         res.json(updatedStation);
     } catch (error) {
-        // Gestion des erreurs
         res.status(500).json({ message: error.message });
     }
 };
 
 // Supprimer une station spécifique
 exports.deleteStation = async (req, res) => {
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Unauthorized: Only administrators can perform this action." });
+    }
     try {
         const { stationId } = req.params;
-
-        // Suppression de la station spécifiée
-        const station = await Station.findByIdAndRemove(stationId);
-
-        if (!station) {
-            return res.status(404).json({ message: "Station not found" });
+        // Vérifiez d'abord si des trains sont associés à cette station
+        const relatedTrains = await Train.find({ $or: [{ start_station: stationId }, { end_station: stationId }] });
+        if (relatedTrains.length > 0) {
+            // Empêchez la suppression si la station est utilisée par des trains
+            return res.status(400).json({
+                message: "Cannot delete station because it is associated with one or more trains."
+            });
         }
-
-        // Envoi de la réponse confirmant la suppression
+        await Station.findByIdAndDelete(stationId);
         res.status(200).json({ message: "Station deleted successfully" });
     } catch (error) {
-        // Gestion des erreurs
         res.status(500).json({ message: error.message });
     }
 };
-
 
 module.exports = {
     createStation,
