969c3a83d7521e0a83d64f268468b99c07c9dac3
BRUN Nicolas <nbrun@meilleurtaux.com>
2023-12-15 10:26:20 +0100

    commentaire trainController.js

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -23,6 +23,7 @@ async function getAllTrains(req, res) {
         query = query.limit(Number(limit));
 
         const trains = await query;
+        // Renvoyer la réponse
         res.json(trains);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -36,9 +37,13 @@ async function createTrain(req, res) {
     }
 
     try {
+        // Récupérer les données du corps de la requête
         const { name, start_station, end_station, time_of_departure } = req.body;
+        // Créer un nouveau train
         const newTrain = new Train({ name, start_station, end_station, time_of_departure });
+        // Enregistrer le train dans la base de données
         const train = await newTrain.save();
+        // Renvoyer la réponse
         res.status(201).json(train);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -48,13 +53,17 @@ async function createTrain(req, res) {
 // Obtenir un train spécifique par son ID
 async function getTrainById(req, res) {
     try {
+        // Récupérer l'ID du train depuis les paramètres de la requête
         const { trainId } = req.params;
+        // Récupérer le train depuis la base de données
         const train = await Train.findById(trainId);
         if (!train) {
             return res.status(404).json({ message: "Train not found" });
         }
+        // Renvoyer la réponse
         res.json(train);
     } catch (error) {
+        // Gérer les erreurs
         res.status(500).json({ message: error.message });
     }
 }
@@ -67,14 +76,19 @@ async function updateTrain(req, res) {
     }
 
     try {
+        // Récupérer l'ID du train depuis les paramètres de la requête
         const { trainId } = req.params;
+        // Récupérer les données de mise à jour depuis le corps de la requête
         const updateData = req.body;
+        // Vérifier si le train existe
         const updatedTrain = await Train.findByIdAndUpdate(trainId, updateData, { new: true });
         if (!updatedTrain) {
             return res.status(404).json({ message: "Train not found" });
         }
+        // Renvoyer la réponse
         res.json(updatedTrain);
     } catch (error) {
+        // Gérer les erreurs
         res.status(500).json({ message: "Server error" });
     }
 }
